30d818391c38be4c4c5753ccef58c950026795e3
Marco Armando Escobar <marcoarmandoea@gmail.com>
2025-08-27 15:25:52 -0600

    Archivo streamlit

---
+++ app.py
@@ -0,0 +1,13 @@
+#Creamos un REPORTE del reto de  estadística inferencial de The learning Gate Data Scientist
+#Plataforma streamlit
+
+#Descargamos la paqueteria necesaria
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+from PIL import Image
+import plotly.express as px
+from dashboard import *
+
